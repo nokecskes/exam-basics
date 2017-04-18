f1d8048e2b35b213029676869ff43dd81eb35da7
Noemi Kecskes <nokecskes@gmail.com>
2017-04-18 09:42:38 +0200

    add example list to calculate add average, add method to do the calculation

--- src/oddavg/OddAvg.java
+++ src/oddavg/OddAvg.java
@@ -1,5 +1,7 @@
 package oddavg;
 
+import java.util.ArrayList;
+
 /**
  * Created by Connor on 2017.04.18..
  */
@@ -10,10 +12,28 @@ public class OddAvg {
     // and returns the average value of the odd numbers in the list
     // Create basic unit tests for it with at least 3 different test cases
 
-
-
-    
+    ArrayList<Integer> numberList = new ArrayList<>();
+    for (int i = 0; i < 10 ; i++) {
+      numberList.add(i + 1);
+    }
+    int oddAverage = oddAverage(numberList);
+    System.out.println(oddAverage);
   }
+
+  public static int oddAverage(ArrayList<Integer> numberList) {
+    int sum = 0;
+    int average = 0;
+    int counter = 0;
+    for (int number : numberList) {
+      if(number % 2 == 1) {
+        sum += number;
+        counter++;
+      }
+    }
+    average = sum / counter;
+    return average;
+  }
+
 
 
 }
